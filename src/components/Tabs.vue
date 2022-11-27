e9572f2a6a5abd058a6505e60f13896baf322add
qiuye <1302580865@qq.com>
2022-11-27 19:23:33 +0800

    updata: tabs style

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -30,7 +30,7 @@ const change = value => {
 </script>
 <template>
     <div class="px-8 py-4">
-        <div class="py-4 px-2 text-lg flex justify-around">
+        <div class="py-4 px-2 text-lg flex justify-around shadow mb-5 rounded-2xl">
             <span
                 v-for="item in TabList"
                 :key="item.path"
